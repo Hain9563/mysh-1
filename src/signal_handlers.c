3eafc46f84422f39ef511bef0c84696f204f2797
You Hye In <yty9323@hanamil.net>
2017-11-14 17:28:40 +0900

    done

--- src/signal_handlers.c
+++ src/signal_handlers.c
@@ -1,11 +1,16 @@
 #include "signal_handlers.h"
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
 
 void catch_sigint(int signalNo)
 {
+   signal(signalNo, SIG_IGN);
   // TODO: File this!
 }
 
 void catch_sigtstp(int signalNo)
 {
+	signal(signalNo, SIG_IGN);
   // TODO: File this!
 }
